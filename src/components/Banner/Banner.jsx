7f86edb40ee1af111c9f3e17e0c3f7520d176700
abutalha08 <taufique752@gmail.com>
2026-04-05 21:14:55 +0600

    Make the banner section responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,19 +6,19 @@ const Banner = () => {
     return (
         <div className='max-w-10/12 mx-auto'>
             <div
-                className="bg-cover bg-center rounded-lg relative overflow-hidden bg-[#0a0a0a]"
+                className="min-h-[400px] bg-cover bg-center rounded-lg relative overflow-hidden bg-[#0a0a0a]"
                 style={{ backgroundImage: `url(${bannerImg})` }}
             >
                 {/* Tailwind Gradient Overlay to fix the "White center" issue */}
                 <div className="absolute inset-0 bg-linear-to-br from-blue-900/20 via-transparent to-pink-900/20 pointer-events-none"></div>
                 
                 {/* Content inside banner (Optional) */}
-                <div className="relative z-10 flex h-full py-10 items-center justify-center">
+                <div className="relative z-10 flex  py-10 items-center justify-center">
                    {/* <h1 className="text-white text-3xl font-bold">Your Content Here</h1> */}
                    <div className='space-y-5 text-center'>
                     <img src={bannerLogo} alt="" className='mx-auto' />
-                    <h2 className="text-white text-3xl font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h2>
-                    <p className='text-[24px] font-medium text-base-200'>Beyond Boundaries Beyond Limits</p>
+                    <h2 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h2>
+                    <p className='text-[18px] md:text-[24px] font-medium text-base-200'>Beyond Boundaries Beyond Limits</p>
                     <button className="btn btn-accent">Claim Free Credit</button>
                    </div>
                 </div>
