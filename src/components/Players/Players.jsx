7e4b9f3ac5c2cdd7523925377f92a498a1784068
abutalha08 <taufique752@gmail.com>
2026-04-12 01:49:39 +0600

    Make the avaiable and selected tab responsive

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -13,11 +13,14 @@ const Players = ({ playersPromise, coins, setCoins }) => {
     const [selectedPlayers, setSelectedPlayers] = useState([]);
 
     return (
-        <div className='max-w-10/12 mx-auto my-16 space-y-6     '>
+        <div className='max-w-10/12 mx-auto px-4 sm:px-6 lg:px-8 my-10 sm:my-16 space-y-6     '>
 
-            <div className='flex justify-between items-center '>
-                {selectedTab === "available" ? <h2 className='font-bold text-3xl'>Available Players</h2> : <h2 className='font-bold text-3xl'>Selected Players ({selectedPlayers.length}/{playersData.length})</h2>}
-                <div>
+            <div className='flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between sm:items-center '>
+
+                {selectedTab === "available" ? <h2 className='font-bold text-2xl sm:text-3xl text-center sm:text-left'>Available Players</h2> : <h2 className='font-bold text-2xl sm:text-3xl text-center sm:text-left'>Selected Players ({selectedPlayers.length}/{playersData.length})</h2>}
+                
+                <div className="flex justify-center sm:justify-end">
+
                     <button onClick={() => setSelectedTab("available")} className={`btn ${selectedTab === "available" ? "bg-[#E7FE29]" : ""}  rounded-r-none rounded-l-xl`}>Available</button>
                     <button onClick={() => setSelectedTab("selected")} className={`btn ${selectedTab === "selected" ? "bg-[#E7FE29]" : ""}  rounded-l-none rounded-r-xl`}>Selected ({selectedPlayers.length})</button>
                 </div>
